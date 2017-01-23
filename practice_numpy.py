2bd0a4b922d54dae6333584833b86ef6f54ae48f
Brian Ishii <brianishii@Brians-MacBook-Pro.local>
2017-01-22 18:18:26 -0800

    practiced numpy lib

--- practice_numpy.py
+++ practice_numpy.py
@@ -1,6 +1,27 @@
 import numpy as np
 
-def test_run():
+def OneD_array():
     print np.array([2,3,4])
+def TwoD_array():
+	print np.array([(2,3,4),(5,6,7)])
+def empty_array():
+    #print np.empty(5)
+    #print np.empty((4,5))
+    print np.empty((5,4,3))
+def ones_array():
+	print np.ones((5,4,3))
+def random_array():
+	print np.random.random((5,4))
+def randInt_array():
+    #print np.random.randint(10)
+    #print np.random.randint(0,10)
+    #print np.random.randint(0,10,size=5)
+    print np.random.randint(0, 10, size=(2,3))
+
 if __name__ == "__main__":
-    test_run()
+    #OneD_array()
+    #TwoD_array()
+    #empty_array()
+    #ones_array()
+    #random_array()
+    #randInt_array()
