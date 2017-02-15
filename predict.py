39cfc19c6c02dc239c436a7506a8a214e9835220
Brian Ishii <brianishii@Brians-MacBook-Pro.local>
2017-02-14 16:33:00 -0800

    added file predict.py with function names

---
+++ predict.py
@@ -0,0 +1,16 @@
+# Predict 
+# Start 2-14-17 
+import trade
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+import updateData
+
+
+def predict_quarter():
+    pass
+def predict_year():
+    pass
+def predict_five_year():
+    pass
