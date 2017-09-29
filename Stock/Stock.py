0b77f08c595b6966de0fbc9bf28adfa45679f721
Brian Ishii <brianishii@Brians-MacBook-Pro.local>
2017-09-29 13:10:41 -0700

    changed file name and added class

--- Stock/Stock.py
+++ Stock/Stock.py
@@ -4,3 +4,14 @@ data.py is an object to look at stock data
 
 Brian Ishii 2017
 """
+
+class Stock:
+    def __init__():
+        pass
+
+    def __repr__():
+        pass
+
+    def __str__():
+        pass
+
