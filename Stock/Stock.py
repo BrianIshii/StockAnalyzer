6154b24d3cab9265fdbc94961c4542affe344c55
Brian Ishii <brianishii@Brians-MacBook-Pro.local>
2017-10-01 11:23:22 -0700

    added stock_test.py and got Stock.py to read json data

--- Stock/Stock.py
+++ Stock/Stock.py
@@ -1,17 +1,24 @@
-#!/usr/bin/env python3
+#/usr/bin/env python3
 """
 data.py is an object to look at stock data
 
 Brian Ishii 2017
 """
+import json
 
 class Stock:
-    def __init__():
+    def __init__(self, symbol):
+        self.symbol = symbol 
+        
+    def __repr__(self):
         pass
 
-    def __repr__():
+    def __str__(self):
         pass
 
-    def __str__():
-        pass
 
+    def get_json_data(self, symbol):
+        f = open("stock.json", 'r')
+        temp = json.load(f)
+        f.close()
+        return temp["Stocks"]["Tech"][symbol]
---
+++ Stock/stock_tests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+stock_tests.py has test for stock.py
+
+Brian Ishii 2017
+"""
+
+import unittest
+
+from stock import *
+class StockTestCases(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
